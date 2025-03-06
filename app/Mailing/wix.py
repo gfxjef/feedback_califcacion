5c63a0f00c175d075e77895b2790c986df2083ad
gfxjef <gfxjef@gmail.com>
2025-03-06 11:12:59 -0500

    Update wix.py

--- app/Mailing/wix.py
+++ app/Mailing/wix.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from .db import get_db_connection  # Ajusta si db.py está en el mismo paquete
+from ..db import get_db_connection  # Ajusta si db.py está en el mismo paquete
 
 wix_bp = Blueprint('wix_bp', __name__)
 TABLE_NAME = "WIX"  # Nombre exacto de la tabla en tu BD
